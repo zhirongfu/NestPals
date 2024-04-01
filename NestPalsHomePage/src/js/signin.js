c0bcde46b84b6cd5228b014e53f1ed86c96fd004
Levi <ZHI.FU57@myhunter.cuny.edu>
2024-04-01 01:10:34 -0400

    added servers, and session cookies and middleware server side token checks, needs testing

--- NestPalsHomePage/src/js/signin.js
+++ NestPalsHomePage/src/js/signin.js
@@ -1,6 +1,51 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"; 
+import { getFirestore, collection, getDocs, query, where } from "firebase/firestore";
+window.addEventListener("DOMContentLoaded", () => {
+const firebaseConfig = {
+    apiKey: "AIzaSyCVrsMKR6f35_JQGglt5bCJaI_wpQkLWWU",
+    authDomain: "nestpals-backend.firebaseapp.com",
+    projectId: "nestpals-backend",
+    storageBucket: "nestpals-backend.appspot.com",
+    messagingSenderId: "377954426735",
+    appId: "1:377954426735:web:92eaef2c3160067572529a"
+};
+firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
 
+          document
+            .getElementById("signin-form")
+            .addEventListener("submit", (event) => {
+              event.preventDefault();
+              const login = event.target.email.value;
+              const password = event.target.password.value;
+
+              firebase
+                .auth()
+                .signInWithEmailAndPassword(login, password)
+                .then(({ user }) => {
+                  return user.getIdToken().then((idToken) => {
+                    return fetch("/sessionLogin", {
+                      method: "POST",
+                      headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json",
+                        "CSRF-Token": Cookies.get("XSRF-TOKEN"),
+                      },
+                      body: JSON.stringify({ idToken }),
+                    });
+                  });
+                })
+                .then(() => {
+                  return firebase.auth().signOut();
+                })
+                .then(() => {
+                  window.location.assign("/profile");
+                });
+              return false;
+            });
+        });
+
+/*
 //our web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyCVrsMKR6f35_JQGglt5bCJaI_wpQkLWWU",
@@ -27,11 +72,13 @@ document.addEventListener("DOMContentLoaded", function() {
         // Retrieve form input values
         const email = document.getElementById("email").value;
         const password = document.getElementById("password").value;
+        firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
 
         try {
             // Authenticate user using Firebase Authentication
             const userCredential = await signInWithEmailAndPassword(auth, email, password);
             const user = userCredential.user;
+            const token = await user.getIdToken();
 
             // If sign-in is successful, alert the user and optionally redirect to another page
             alert("Sign in successful!");
@@ -46,3 +93,4 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+*/
